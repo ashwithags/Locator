8f04bc7294f69f82fa68537933e7e6ad5f45a6fc
ankantechninzaz <techninzazankan@gmail.com>
2018-02-16 23:53:58 +0530

    Added code for dynamic color

--- js/controller.js
+++ js/controller.js
@@ -53,16 +53,16 @@ angular.module('LocatorApp.controllers', [])
 	enquiry.getContactedList().success(function(wow){
    		if(wow.success){
    			$scope.contacted = wow.cnt_list;
-   			console.log(contacted);
+   			//console.log(contacted);
    		}
    	});
    	enquiry.getStudentsList().success(function(info){
 		$scope.StudentsList = info;
-		console.log(StudentsList);
+		//console.log(StudentsList);
    	});
    	enquiry.currentPosition().success(function(data){
 		$scope.position = data;
-		console.log(position);
+		//console.log(position);
    	});
 })
 
--- js/directive.js
+++ js/directive.js
@@ -9,16 +9,12 @@ angular.module('LocatorApp.directive',[])
 		templateUrl: 'pages/left-sidebar.html'
 	};
 })
-.directive("randomBackgroundcolor", function () {
+.directive("randomColor", function () {
     return {
-        restrict: 'EA',
+        restrict: 'C',
         replace: false,
         link: function (scope, div, attr) {
-
-            //generate random color
             var color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16); 
-            
-            //Add random background class to selected element
             div.css('background-color', color);
 
         }
